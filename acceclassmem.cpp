04e60906ccc8678a0a92e7d0c54f24970bac3dc1
aish349 <71930826+aish349@users.noreply.github.com>
2020-10-13 23:25:19 +0530

    created program to access class members

---
+++ acceclassmem.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class items
+{
+private :
+int number;
+float price;
+int qty;
+void values(void);
+public :
+void show();
+};
+void items::show(void)
+{
+values();
+}
+void items::values(void)
+{
+number=123;
+price=56.34;
+qty=50;
+cout<<"number:"<<number<<endl;
+cout<<"price :"<<price<<endl;
+cout<<"qty :"<<qty<<endl;
+};
+int main()
+{
+items ob;
+ob.show();
+return 0;
+}
